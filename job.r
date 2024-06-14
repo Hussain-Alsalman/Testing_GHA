b6a28433c5a319f3663ffd18335f5dc0fac9fddc
Hussain Alsalman <Alsalman.hn@gmail.com>
2024-06-15 00:19:40 +0300

    Update job.r

--- job.r
+++ job.r
@@ -1,4 +1,6 @@
 library("tasi")
+library("dplyr")
+library("ggplot2")
 df <- get_index_records("2021-01-01","2022-12-31")
  plt1 <- df %>%  ggplot(mapping = aes(x = as.POSIXct(transactionDate), y = previousClosePrice)) + geom_line() + 
   labs(title = "Historical data for tasi index 2021-2022", x = "Date", y = "TASI index") + theme_classic()
